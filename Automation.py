d5bc338c135fa2b31d8064fe9e822276f617b5d5
umang rajpara <umangrajpara99@gmail.com>
2021-03-01 09:29:12 +0100

    create and save new app

--- Automation.py
+++ Automation.py
@@ -3,12 +3,13 @@ import sys
 from PyQt5 import QtWidgets, QtCore
 
 
-app = QtWidgets.QApplication(sys.argv)
-Automate = Ui_AutomationWindow()
-Automate.setupUi()
-Automate.addToolBar(QtCore.Qt.BottomToolBarArea, Automate.toolBar)
-Automate.toolBar.setAllowedAreas(QtCore.Qt.BottomToolBarArea|QtCore.Qt.TopToolBarArea)
-Automate.move(650,150)
-Automate.show()
+if __name__ == '__main__':
+	app = QtWidgets.QApplication(sys.argv)
+	Automate = Ui_AutomationWindow()
+	Automate.setupUi()
+	Automate.addToolBar(QtCore.Qt.BottomToolBarArea, Automate.toolBar)
+	Automate.toolBar.setAllowedAreas(QtCore.Qt.BottomToolBarArea|QtCore.Qt.TopToolBarArea)
+	Automate.move(650,150)
+	Automate.show()
 
-app.exec_()
+	app.exec_()
